c31175aaa5cc8322938a78b9e817e09cd1bbd885
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-09 20:38:23 +0430

    redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { Button } from "./components/ui/button";
+import { useAppSelector } from "./hooks";
 
 function App() {
+  const { name } = useAppSelector((state) => state.userState);
+  console.log(name);
+  
   return (
     <div>
-      <Button variant="secondary" size="lg" onClick={() => console.log("Hello")}>
+      <Button
+        variant="secondary"
+        size="lg"
+        onClick={() => console.log("Hello")}
+      >
         Click Me
       </Button>
       <h2 className="text-red-900 p-3">Hello People</h2>;
