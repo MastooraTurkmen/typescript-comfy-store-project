182835d518c5016a7e4f58c6944a93f870c7019f
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-16 21:23:22 +0430

    home layout

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 function HomeLayout() {
   return (
     <>
-      <Outlet />
+      <Header />
+      <div className="align-element py-20">
+        <Outlet />
+      </div>
     </>
   );
 }
