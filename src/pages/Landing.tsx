804f08952ab7b96c7b4c043f5de03b1d17d251e2
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-20 20:50:40 +0430

    landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,21 @@
 import { useLoaderData, type LoaderFunction } from "react-router-dom";
 import { Hero, FeaturedProduct } from "../components";
+import { customFetch } from "../utils";
+import { ProductsResponse } from "../utils/types";
 
-export const loader: LoaderFunction = async () => {
-  console.log("loading landing page");
-  return null;
+const url = "/products?featured=true";
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+  const response = await customFetch<ProductsResponse>(url);
+  console.log(response);
+  return { ...response.data };
 };
 
 function Landing() {
+  const result = useLoaderData() as ProductsResponse;
+  console.log(result);
+
   return (
     <>
       <Hero />
