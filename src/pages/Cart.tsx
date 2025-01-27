2775b67171d53805eba707254a4077a00bfb8ce1
Mastoora Turkmen <janmastoorajan@gmail.com>
2025-01-27 21:31:41 +0430

    cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,19 +1,40 @@
+import { useAppSelector } from "../hooks";
+import { CartItemsList, SectionTitle, CartTotals } from "../components";
 import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
 
 function Cart() {
+  // temp
+  const user = null;
+  const numItemsInCart = useAppSelector(
+    (state) => state.cartState.numItemsInCart
+  );
+
+  if (numItemsInCart === 0) {
+    return <SectionTitle text="Empty Cart" />;
+  }
+
   return (
-    <div>
-      <h1 className="text-4xl">Cart Page</h1>
-      <Link to="/" className="text-7xl text-red-900">
-        back home
-      </Link>
-      <div>
-        <Button asChild size="lg">
-          <Link to="/">Home</Link>
-        </Button>
+    <>
+      <SectionTitle text="Shopping Cart" />
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals />
+          {user ? (
+            <Button asChild className="mt-8 w-full">
+              <Link to="/checkout">Proceed to checkout</Link>
+            </Button>
+          ) : (
+            <Button asChild className="mt-8 w-full">
+              <Link to="/login">Please Login</Link>
+            </Button>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 export default Cart;
